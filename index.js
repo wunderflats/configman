61c270186714af31df063aaca393f57f236383a0
Danny Arnold <despair.blue@gmail.com>
2016-02-10 14:27:45 +0100

    initial commit

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-require('babel-register')({
-  presets: ['es2015-node5'],
-  plugins: ['transform-object-rest-spread'],
-  sourceMaps: 'both',
-  sourceRoot: 'yawp'
-})
-require('./lib/index')
