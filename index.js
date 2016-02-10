160541b62642d8a2919d8104f939b19929aaa8c3
Danny Arnold <despair.blue@gmail.com>
2016-02-10 14:27:45 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-global.Promise = require('bluebird')
-global.Promise.longStackTraces
 require('babel-register')({
   presets: ['es2015-node5'],
   plugins: ['transform-object-rest-spread'],
