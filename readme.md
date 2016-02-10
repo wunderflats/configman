fe6a91f2938304cd3fc36c0b46f2ba26247f820d
Danny Arnold <despair.blue@gmail.com>
2016-02-10 14:27:45 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,79 @@
+# configman
+
+Tool for defining and checking environment variables in one place.
+
+```
+npm install @wunderflats/configman
+```
+
+## Usage
+
+``` js
+const envVars = ['PORT', 'HOSTNAME']
+
+const config = require('@wunderflats/configman')
+  .init(envVars)
+  .ensureAllSet()
+
+http
+  .createServer()
+  .listen(config.PORT, config.HOSTNAME)
+```
+
+# API
+
+#### require
+
+`configman = require('@wunderflats/configman')`
+
+Returns an object with the methods:
+
+* `init : Array String -> Object`
+* `ensureAllSet -> Object`
+* `get -> Object`
+
+#### inti
+
+`config.init : Array String -> Object`
+
+Takes an array of strings and returns the initial configman object again, thus can be chained with `get()` or `ensureAllSet`.
+
+The array tells configman what environment variables should be set.
+
+```js
+config.init(['PORT'])
+```
+
+#### ensureAllSet
+
+`config.ensureAllSet : Object`
+
+Returns an object containing properties for all configured environment variables.
+
+It also checks if all environment variable are set and throws if not.
+
+```js
+config = configman
+  .init('PORT')
+  .ensureAllSet() // throws if `PORT` is not set (part of `process.env`)
+```
+
+#### get
+
+`config.get() : Object`
+
+Returns an object containing properties for all configured environment variables.
+
+Throws if one of those variables is not set (part of `process.env`) when accessed.
+
+```js
+config = configman
+  .init('PORT')
+  .get()
+
+config.PORT // throws if PORT is not set (part of `process.env`)
+```
+
+## License
+
+Unlicensed.
---
+++ test/index.js
@@ -0,0 +1,98 @@
+/* eslint-disable no-process-env */
+'use strict'
+
+const test = require('tape')
+const config = require('../lib/index')
+
+test('config', (t) => {
+  test('`ensureAllSet` throws and exceptions if called before init', (t) => {
+    // setup
+    t.plan(1)
+
+    // test
+    t.throws(config.ensureAllSet, /init\(\) must be called before ensureAllSet\(\)/)
+  })
+
+  test('`get` throws and exceptions if called before init', (t) => {
+    // setup
+    t.plan(1)
+
+    // test
+    t.throws(config.get, /init\(\) must be called before get\(\)/)
+  })
+
+  test('`init` returns the initial module', (t) => {
+    // setup
+    t.plan(1)
+
+    // test
+    t.equals(config.init([]), config)
+  })
+
+  test('defines only the properties it is asked to define', (t) => {
+    // setup
+    t.plan(2)
+
+    delete process.env.ICEKING
+    config.init([
+      'PATH',
+      'PORT'
+    ])
+
+    // test
+    t.true(config.get().hasOwnProperty('PORT'), 'PATH defined')
+    t.false(config.get().hasOwnProperty('ICEKING'), 'ICEKING is undefined')
+  })
+
+  test('throws if a property is not backed by an env var', (t) => {
+    // setup
+    t.plan(1)
+
+    delete process.env.ICEKING
+    config.init(['ICEKING'])
+
+    // test
+    t.throws(() => config.get().ICEKING, /ENVIRONMENT VARIABLE ICEKING IS NOT SET!/)
+  })
+
+  test('throws if a `ensureAllSet` is called and a property is not backed by an env var', (t) => {
+    // setup
+    t.plan(1)
+
+    delete process.env.ICEKING
+    config.init(['ICEKING'])
+
+    // test
+    t.throws(config.ensureAllSet, /ENVIRONMENT VARIABLE ICEKING IS NOT SET!/)
+  })
+
+  test('returns the env var corresponding to a property', (t) => {
+    // setup
+    t.plan(1)
+
+    process.env.FINN = 'jake'
+    config.init(['FINN'])
+
+    // test
+    t.equal(config.get().FINN, 'jake')
+
+    // tear down
+    delete process.env.FINN
+  })
+
+  test('does not throw if `ensureAllSet` is called and all properties are backed by an env var', (t) => {
+    // set up
+    t.plan(1)
+
+    process.env.FINN = 'jake'
+    config.init(['FINN'])
+
+    // test
+    t.doesNotThrow(config.ensureAllSet)
+
+    // tear down
+    delete process.env.FINN
+  })
+
+  t.end()
+})
