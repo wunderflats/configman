6d960f5998f5e85a1c10494ce342c2f7c7f2d448
Johnny Lee-Othon <jleeothon@icloud.com>
2024-05-28 13:12:46 +0200

    Do not depend on tap; use node:test

--- test/index.js
+++ test/index.js
@@ -1,71 +1,62 @@
-/* @flow */
 /* eslint-disable no-process-env */
 'use strict'
 
-const test = require('tap').test
 const configman = require('../lib/index')
+const assert = require('node:assert')
+const { describe, it } = require('node:test')
 
-test('config', (t) => {
-  test('`ensureAllSet` returns configman', (t) => {
+describe('config', () => {
+  it('`ensureAllSet` returns configman', () => {
     // setup
-    t.plan(1)
-
     process.env.PORT = '1337'
 
     const config = configman.ensureAllSet(['PORT'])
 
     // test
-    t.deepEqual(config, configman)
+    assert.deepStrictEqual(config, configman)
 
     // tear down
     delete process.env.PORT
   })
 
-  test('throws if a property is not backed by an env var', (t) => {
+  it('throws if a property is not backed by an env var', () => {
     // setup
-    t.plan(1)
-
     delete process.env.ICEKING
 
     // test
-    t.throws(() => configman.get('ICEKING'), /environment variable iceking is not set!/i)
+    assert.throws(() => configman.get('ICEKING'), /environment variable iceking is not set!/i)
   })
 
-  test('throws if a `ensureAllSet` is called and a property is not backed by an env var', (t) => {
+  it('throws if a `ensureAllSet` is called and a property is not backed by an env var', (t) => {
     // setup
-    t.plan(1)
-
     delete process.env.ICEKING
 
     // test
-    t.throws(() => configman.ensureAllSet(['ICEKING']), /environment variable iceking is not set!/i)
+    assert.throws(
+      () => configman.ensureAllSet(['ICEKING']),
+      /environment variable iceking is not set!/i
+    )
   })
 
-  test('returns the env var corresponding to a property', (t) => {
+  it('returns the env var corresponding to a property', () => {
     // setup
-    t.plan(1)
-
     process.env.FINN = 'jake'
 
     // test
-    t.equal(configman.get('FINN'), 'jake')
+    assert.strictEqual(configman.get('FINN'), 'jake')
 
     // tear down
     delete process.env.FINN
   })
 
-  test('does not throw if `ensureAllSet` is called and all properties are backed by an env var', (t) => {
+  it('does not throw if `ensureAllSet` is called and all properties are backed by an env var', (t) => {
     // set up
-    t.plan(1)
-
     process.env.FINN = 'jake'
 
     // test
-    t.doesNotThrow(() => configman.ensureAllSet(['FINN']))
+    assert.doesNotThrow(() => configman.ensureAllSet(['FINN']))
 
     // tear down
     delete process.env.FINN
   })
-
-  t.end()
 })
